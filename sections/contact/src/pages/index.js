fcd810e67eda5ea89bfc4f160a2cb3089dd807f9
unknown <ernestweems@gmail.com>
2020-11-07 15:50:25 -0500

    Form hooks next add form states

---
+++ sections/contact/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './global.css';
+import Form from '../components/form';
+
+
+export default () => (
+  <>
+    <h1>Send a Message!</h1>
+   
+    <Form/>
+   
+  </>
+);
